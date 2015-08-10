3dbf164ff97a2bd020eafcdc1865428042860378
yuzutas0 <sho.y0921@gmail.com>
2015-08-10 19:04:22 +0900

    Event一覧表示：source_idによる絞り込み

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,9 @@
 class EventsController < ApplicationController
 
 	def index
-		@events = Event.page(params[:page]).per(50).order(:source_updated_at).reverse_order
+		source_list = [params[:source_id]] if params[:source_id].present?
+		source_list = 1..4 if source_list.blank?
+		@events = Event.where("source_id IN (?)", source_list).page(params[:page]).per(50).order(:source_updated_at).reverse_order
 	end
 
 	def show
