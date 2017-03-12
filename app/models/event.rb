42413d8e1303da2e9e16c689189850a473e55a7a
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-03-12 21:12:24 +0900

    formatter

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,18 +1,18 @@
 class Event < ActiveRecord::Base
-	include SearchableEvent
+  include SearchableEvent
 
-	def self.create_index
-		self.__elasticsearch__.client = Elasticsearch::Client.new host: "https://host:port" # TODO: set value
-		self.__elasticsearch__.client.indices.delete index: self.index_name rescue nil
-		self.create_index! force: true
-		self.__elasticsearch__.import
-	end
+  def self.create_index
+    self.__elasticsearch__.client = Elasticsearch::Client.new host: 'https://host:port' # TODO: set value
+    self.__elasticsearch__.client.indices.delete index: self.index_name rescue nil
+    self.create_index! force: true
+    self.__elasticsearch__.import
+  end
 
-	def self.show_index(query, page, per)
-		response = self.search(query).page(page).per(per).records
-	end
+  def self.show_index(query, page, per)
+    response = self.search(query).page(page).per(per).records
+  end
 
-	def more_like_this!
-		self.more_like_this().records
-	end
+  def more_like_this!
+    self.more_like_this().records
+  end
 end
