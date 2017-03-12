88073826799fcaa64be35f8c75f97ddb760d4fc0
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-03-12 21:11:42 +0900

    formatter

--- lib/crawls/robots/connpass.rb
+++ lib/crawls/robots/connpass.rb
@@ -9,20 +9,20 @@ class Crawls::Robots::Connpass
 
   # rails runner Crawls::Robots::Connpass.execute
   def self.execute
-    puts "Connpass"
+    puts 'Connpass'
 
     # loop : yymm (e.g. 201508 - 201512)
     date = Date.today
     for after_month in 0..4
 
       # loop : start (1, 101, 201, ..., last)
-      date_string = (date >> after_month).strftime("%Y%m")
+      date_string = (date >> after_month).strftime('%Y%m')
       start_count = 1
       get_count = 100
       loop do
 
         # HTTP
-        request_uri = "http://connpass.com/api/v1/event/?ym=" + date_string + "&count=" + get_count.to_s + "&start=" + start_count.to_s
+        request_uri = 'http://connpass.com/api/v1/event/?ym=' + date_string + '&count=' + get_count.to_s + '&start=' + start_count.to_s
         response = open(request_uri, &:read).toutf8
         sleep(2)
 
@@ -41,7 +41,7 @@ class Crawls::Robots::Connpass
 
           # find same event
           next if new_event.source_id.blank? || new_event.source_event_id.blank?
-          find_event_query = "source_id = :source_id AND source_event_id = :source_event_id"
+          find_event_query = 'source_id = :source_id AND source_event_id = :source_event_id'
           old_events = Event.where(find_event_query, source_id: new_event.source_id, source_event_id: new_event.source_event_id)
           old_event = old_events[0] if old_events.present?
 
