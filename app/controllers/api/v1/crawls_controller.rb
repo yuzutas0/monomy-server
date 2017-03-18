51a98628c99a56c66976273906911778f0a58583
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-03-18 17:32:38 +0900

    formatter

--- app/controllers/api/v1/crawls_controller.rb
+++ app/controllers/api/v1/crawls_controller.rb
@@ -1,37 +1,38 @@
 module Api
-	module V1
-		class CrawlsController < ApplicationController
+  module V1
+    class CrawlsController < ApplicationController
 
-			def execute
-				id = params[:id]
-				started_at = Time.now
-				message = "OK"
+      def execute
+        id = params[:id]
+        started_at = Time.now
+        message = "OK"
 
-				begin
-					Event.create_index                 if id == "100"
-					Crawls::Robots::Atnd.execute       if id == "1"
-					Crawls::Robots::Doorkeeper.execute if id == "2"
-					Crawls::Robots::Zusaar.execute     if id == "3"
-					Crawls::Robots::Connpass.execute   if id == "4"
-				rescue => e
-					message = e.message
-				end
+        begin
+          Event.create_index if id == "100"
+          Crawls::Robots::Atnd.execute if id == "1"
+          Crawls::Robots::Doorkeeper.execute if id == "2"
+          Crawls::Robots::Zusaar.execute if id == "3"
+          Crawls::Robots::Connpass.execute if id == "4"
+        rescue => e
+          message = e.message
+        end
 
-				finished_at = Time.now
-				response = Response.new(message, started_at, finished_at)
-				render json: response
-			end
+        finished_at = Time.now
+        response = Response.new(message, started_at, finished_at)
+        render json: response
+      end
 
-			private
-				class Response
-					def initialize(message, started_at, finished_at)
-						@message = message
-						@started_at = started_at
-						@finished_at = finished_at
-					end
-					attr_accessor :message, :started_at, :finished_at
-				end
+      private
+      class Response
+        def initialize(message, started_at, finished_at)
+          @message = message
+          @started_at = started_at
+          @finished_at = finished_at
+        end
 
-		end
-	end
+        attr_accessor :message, :started_at, :finished_at
+      end
+
+    end
+  end
 end
